5d4ccc658ea0489ad6036d5e8b0650dc78509565
highcowoo <highcowoo@gmail.com>
2021-01-06 15:12:59 +0900

    유니티 1주차 2일차 학습

---
+++ Assets/script/followplayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followplayer : MonoBehaviour
+{
+    [Header("따라갈대상지정")]
+    [SerializeField] Transform tf_player;
+
+    [Header("따라갈 속도 지정")] [Range(0,1)]
+    [SerializeField] float speed;
+
+    Vector3 currentPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentPos = tf_player.position - transform.position; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, tf_player.position-currentPos , speed);
+
+    }
+}
