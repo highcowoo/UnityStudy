a1b97dfcc772da8ec99c1258cef7dd45c6d5a3e7
highcowoo <highcowoo@gmail.com>
2021-01-14 14:06:38 +0900

    유니티스터디 5일차

---
+++ Assets/script/bullit.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullit : MonoBehaviour
+{
+    [Header("피격이펙트")]
+    [SerializeField] GameObject go_Rico;
+    [Header("총알데미지")]
+    [SerializeField] int damage;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        ContactPoint contactPoint = other.contacts[0];
+        var clone = Instantiate(go_Rico, contactPoint.point, Quaternion.LookRotation(contactPoint.normal));
+        Destroy(clone, 0.5f);
+        Destroy(gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
